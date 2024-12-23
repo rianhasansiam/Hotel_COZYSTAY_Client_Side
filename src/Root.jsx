0f7ebcb66f1dcf8ac71d203b0489c7d860b840e1
rianhasansiam <rianhasan1971@gmail.com>
2024-12-23 23:34:22 +0600

    room component added

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Error from "./Error/Error";
+import Home from "./Home/Home";
 // import Home from "../Pages/Home/Home";
 // import Error from "../Shared/Error/Error";
 // import Login from "../Pages/Login/Login";
